b0e24a08a7aaf1d4628b5dfffc326ebd89f9c26f
PeterSaan <saanpeter@gmail.com>
2025-03-22 22:24:09 +0200

    Add response code and curlrc

--- tests.sh
+++ tests.sh
@@ -1,19 +1,43 @@
 #!/usr/bin/env bash
 
 URL="https://petstore.swagger.io/v2"
-API_KEY="rand0m455keyImad3"
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NO_COLOR='\033[0m'
 
 AddPet() {
-	curl -v -o AddPet.json -X POST \
+	curl -K .curlrc --fail-with-body -o AddPet.json -X POST \
 		"$URL/pet" \
-		-H 'Content-Type: application/json' \
-		-H "api_key: $API_KEY" \
 		-d '
 		{
 			"name": "Petsi dawg",
+			"photoUrls": ["string"],
+			"tags": [
+				{ "name": "big" },
+				{ "name": "fast" },
+				{ "name": "terrifying" }
+			],
+			"status": "unavailable"
+		}
+			 '
+
+		if grep -q "code" AddPet.json; then
+			echo -e "${RED}AddPet test failed${NO_COLOR}"
+		else
+			echo -e "${GREEN}AddPet test successful${NO_COLOR}"
+		fi
+}
+
+FindPetById() {
+	curl -K .curlrc --fail-with-body -o FindPetById.json -X POST \
+		"$URL/pet" \
+		-d '
+		{
+			"name": "Petsi test dawg",
 			"photoUrls": ["string"]
 		}
 			 '
 }
 
-AddPet | jq
+AddPet
+#FindPetById
